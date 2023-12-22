8d83b0fa7ccfcda2ec91bce5ff35115a46d60040
joannafg <joannafg@vt.edu>
2023-12-22 11:42:17 -0500

    added /submit

--- server.js
+++ server.js
@@ -8,10 +8,19 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json()); // Middleware to parse JSON requests
 
-// Example route
+// Test route
 app.get('/', (req, res) => {
-  res.send('Hello World we0g8! Successfully fetched from backend : )');
+  res.send('Successfully fetched from backend : )');
 });
+
+app.post('/submit', (req, res) => {
+  const userData = req.body;
+  console.log(userData);  // Process or log the data as needed
+
+  // Send a response back to the frontend
+  res.json({ req: req.body, message: "Data received successfully!" });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
