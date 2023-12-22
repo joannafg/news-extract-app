180accd4fe7ba9fa746ff6cc4c22a4db75650a0f
joannafg <joannafg@vt.edu>
2023-12-22 13:42:46 -0500

    testing

--- server.js
+++ server.js
@@ -6,56 +6,47 @@ const openai = new OpenAIApi({
   apiKey: process.env.OPENAI_API_KEY
 });
 
-
 const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3001;
-
 app.use(cors());
-app.use(express.json()); // Middleware to parse JSON requests
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
 
 // Test route
 app.get('/', (req, res) => {
   res.send('Successfully fetched from backend : )');
 });
 
-// app.post('/submit', (req, res) => {
-//   const userData = req.body;
-//   console.log(userData);  // Process or log the data as needed
-
-//   // Send a response back to the frontend
-//   res.json({ receivedData: req.body, message: "Data received successfully!" });
-// });
-
 app.post('/submit', async (req, res) => {
   const userData = req.body;
   console.log(userData);  // Log the user data
 
   try {
-    // Call OpenAI's API
-    const openaiResponse = await openai.createCompletion({
-      model: "text-davinci-003", // You can choose a different model as needed
-      prompt: "Say 'Hello World' in a creative way based on this input: " + userData.input,
-      max_tokens: 50
+    // Call OpenAI's Chat API
+    const openaiResponse = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo", // Using the ChatGPT model
+      messages: [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": userData.input} // User's input
+        // You can add more messages if needed for context
+      ]
     });
 
     // Send a response back to the frontend
     res.json({ 
-      receivedData: req.body, 
+      receivedData: userData, 
       message: "Data received successfully!", 
-      openaiResult: openaiResponse.data.choices[0].text.trim() 
+      openaiResult: openaiResponse.data.choices[0].message.content 
     });
   } catch (error) {
     console.error('OpenAI API error:', error);
     res.status(500).send('Error processing data with OpenAI');
   }
 });
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
