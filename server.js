bd95182c681799c7dd8cc9602f27eb4a12e0ab7f
joannafg <joannafg@vt.edu>
2023-12-22 13:28:08 -0500

    back to hello world

--- server.js
+++ server.js
@@ -15,35 +15,35 @@ app.get('/', (req, res) => {
   res.send('Successfully fetched from backend : )');
 });
 
-// app.post('/submit', (req, res) => {
-//   const userData = req.body;
-//   console.log(userData);  // Process or log the data as needed
+app.post('/submit', (req, res) => {
+  const userData = req.body;
+  console.log(userData);  // Process or log the data as needed
 
-//   // Send a response back to the frontend
-//   res.json({ receivedData: req.body, message: "Data received successfully!" });
-// });
-
-app.post('/submit', async (req, res) => {
-  try {
-    const userData = req.body;
-    const links = req.body.req.inputs[0]
-    console.log(req.body.inputs[0])
-    // const response = await openai.createCompletion({
-    //   model: "text-davinci-003", // Or whichever model you intend to use
-    //   prompt: "Summarize the webiste" + userData.text, 
-    //   max_tokens: 150,
-    // });
-
-    res.json({ 
-      originalData: userData, 
-      testing: "testing0", 
-      openaiResponse: response.data.choices[0].text.trim()
-    });
-  } catch (error) {
-    console.error('Error with OpenAI API:', error);
-    res.status(500).send('Error processing data with OpenAI');
-  }
+  // Send a response back to the frontend
+  res.json({ receivedData: req.body, message: "Data received successfully!" });
 });
+
+// app.post('/submit', async (req, res) => {
+//   try {
+//     const userData = req.body;
+//     const links = req.body.req.inputs[0]
+//     console.log(req.body.inputs[0])
+//     const response = await openai.createCompletion({
+//       model: "text-davinci-003", // Or whichever model you intend to use
+//       prompt: "Summarize the webiste" + userData.text, 
+//       max_tokens: 150,
+//     });
+
+//     res.json({ 
+//       originalData: userData, 
+//       testing: "testing0", 
+//       openaiResponse: response.data.choices[0].text.trim()
+//     });
+//   } catch (error) {
+//     console.error('Error with OpenAI API:', error);
+//     res.status(500).send('Error processing data with OpenAI');
+//   }
+// });
 
 
 
