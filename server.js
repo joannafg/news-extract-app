464a6fce2c6a11f33ee4571b677f06b864852723
joannafg <joannafg@vt.edu>
2023-12-22 13:31:29 -0500

    openai added?

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 //app-name: pacific-stream-59101
 
+const { Configuration, OpenAIApi } = require('openai');
+
+const openai = new OpenAIApi(new Configuration({
+  apiKey: process.env.OPENAI_API_KEY, // Ensure your API key is stored in .env file
+}));
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -15,35 +21,38 @@ app.get('/', (req, res) => {
   res.send('Successfully fetched from backend : )');
 });
 
-app.post('/submit', (req, res) => {
-  const userData = req.body;
-  console.log(userData);  // Process or log the data as needed
+// app.post('/submit', (req, res) => {
+//   const userData = req.body;
+//   console.log(userData);  // Process or log the data as needed
 
-  // Send a response back to the frontend
-  res.json({ receivedData: req.body, message: "Data received successfully!" });
+//   // Send a response back to the frontend
+//   res.json({ receivedData: req.body, message: "Data received successfully!" });
+// });
+
+app.post('/submit', async (req, res) => {
+  const userData = req.body;
+  console.log(userData);  // Log the user data
+
+  try {
+    // Call OpenAI's API
+    const openaiResponse = await openai.createCompletion({
+      model: "text-davinci-003", // You can choose a different model as needed
+      prompt: "Say 'Hello World' in a creative way based on this input: " + userData.input,
+      max_tokens: 50
+    });
+
+    // Send a response back to the frontend
+    res.json({ 
+      receivedData: req.body, 
+      message: "Data received successfully!", 
+      openaiResult: openaiResponse.data.choices[0].text.trim() 
+    });
+  } catch (error) {
+    console.error('OpenAI API error:', error);
+    res.status(500).send('Error processing data with OpenAI');
+  }
 });
 
-// app.post('/submit', async (req, res) => {
-//   try {
-//     const userData = req.body;
-//     const links = req.body.req.inputs[0]
-//     console.log(req.body.inputs[0])
-//     const response = await openai.createCompletion({
-//       model: "text-davinci-003", // Or whichever model you intend to use
-//       prompt: "Summarize the webiste" + userData.text, 
-//       max_tokens: 150,
-//     });
-
-//     res.json({ 
-//       originalData: userData, 
-//       testing: "testing0", 
-//       openaiResponse: response.data.choices[0].text.trim()
-//     });
-//   } catch (error) {
-//     console.error('Error with OpenAI API:', error);
-//     res.status(500).send('Error processing data with OpenAI');
-//   }
-// });
 
 
 
